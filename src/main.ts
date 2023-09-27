0c6ce3f4516d9206eb91cfb992c649e7b3774f2d
Florent Cardoen <f.cardoen@me.com>
2023-09-27 09:37:03 +0200

    feat: new notfification

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule, { bufferLogs: true });
 	app.useLogger(app.get(Logger));
 
-	await app.listen(process.env.PORT || 3000);
+	await app.listen(process.env.NOTIFICATION_PORT || 3000);
 
 }
 
